67db972ae6b5ddff5e47dcaaed965d71594b282b
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-01 22:59:46 +0100

    added props to the Pokecard component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import Pokecard from './Pokecard'
 
 function App() {
   return (
-    <div className="App" />
-  );
+    <div className="App">
+      <Pokecard
+        id={4}
+        name="Charmander"
+        type="fire"
+        exp={62}
+      />
+    </div>
+  )
 }
 
 export default App;
