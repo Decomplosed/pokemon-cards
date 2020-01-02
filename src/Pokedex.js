8bf74043123e2661bbf679a59d8066129d3108fc
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-02 16:10:48 +0100

    added conditional to write which hand wins

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -21,6 +21,7 @@ class Pokedex extends Component {
       <div className="Pokedex">
         <h1>Pokedex!</h1>
         <p>Total Exp: {this.props.exp}</p>
+        <p>{this.props.isWinner ? 'WINNER!' : 'LOSER!'}</p>
         <div className="Pokedex-cards">
           {this.props.pokemon.map(p => (
             <Pokecard
