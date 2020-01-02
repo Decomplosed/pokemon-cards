ae68d44efeb424100bfd713f899c0306effd6d28
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-02 16:14:41 +0100

    created a title variable which checks if lose or win and create a proper title with proper classname

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -17,11 +17,19 @@ class Pokedex extends Component {
   }
 
   render() {
+    let title
+
+    if (this.props.isWinner) {
+      title = <h1 className="Pokedex-winner">Winning Hand</h1>
+    } else {
+      title = <h1 className="Pokedex-loser">Losing Hand</h1>
+    }
+
     return (
       <div className="Pokedex">
         <h1>Pokedex!</h1>
         <p>Total Exp: {this.props.exp}</p>
-        <p>{this.props.isWinner ? 'WINNER!' : 'LOSER!'}</p>
+        {title}
         <div className="Pokedex-cards">
           {this.props.pokemon.map(p => (
             <Pokecard
